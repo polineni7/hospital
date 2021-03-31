dc8de33b9e1dcb65231cf0b3a27346585dad020e
polineni7 <imrampolineni@gmail.com>
2021-03-31 20:29:28 +0530

    Ram 1st

---
+++ WebApplication1/Controllers/PatientController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class PatientController : Controller
+    {
+       [HttpGet]
+       [Route('api/dept/GetPatients')]
+    }
+}
